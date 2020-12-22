941c671160bc9fb6ea1012d1bc6e10f7af482a6d
Gabriel Piqueras <rocamora_gabriel@hotmail.com>
2020-12-22 02:46:38 +0100

    Implementación de buscador de canciones con diseño de Bulma y alguna que otra funcionalidad básica.

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,55 @@
 <template lang="pug">
   
   #app
-    
+    section.section
+      nav.nav.has-shadow
+        .container
+          input.input.is-large(type="text" placeholder="Buscar canciones" v-model="searchQuery")
+          a.button.is-info.is-large.mt-4(v-on:click="search") Buscar
+          a.button.is-danger.is-large.mt-4.ml-3 &times;
+          p
+            small {{ searchMessage }}
+            
+        .container.results
+          .columns
+            .column(v-for="t in tracks") {{ t.name }} - {{ t.artist}}
 </template>
 
 <script>
+
+const tracks = [
+  { name: 'Muchacha', artist:'Luis Alberto Spinetta' },  
+  { name: 'Hoy acá en el baile', artist:'El pepo' },
+  { name: 'I was made for loving you', artist:'Kiss' } 
+]
 
 export default {
   name: 'app',
   data () {
     return {
-      
+      searchQuery: '',
+      tracks: []
+    }
+  },
+  computed: {
+    searchMessage() {
+      return `Encontrados: ${this.tracks.length}`;
+    }
+  },
+  methods: {
+    search() {
+      this.tracks = tracks;
+      this.results = tracks.length;
+      console.log(this.searchQuery);
     }
   }
 }
 </script>
 
 <style lang="scss">
+
+body {
+  background: rgb(151, 151, 151);
+  height: 100%;
+}
 </style>
